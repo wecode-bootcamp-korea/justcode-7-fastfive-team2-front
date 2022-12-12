7dcea1cb3cdaa9967adfeca97609d9187b6471fa
Youje0 <xhdckd12@gmail.com>
2022-12-12 18:32:56 +0900

    <modify> Router 오류해결

---
+++ src/pages/Post/Post.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Post.scss';
+
+function Post() {
+  return <div>해이</div>;
+}
+
+export default Post;
--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './Home';
+import Post from './Post/Post';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Post />} />
       </Routes>
     </BrowserRouter>
   );
