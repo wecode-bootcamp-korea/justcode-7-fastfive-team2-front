be4845efb2d24db8da4c1a72a32ee04f19c5b327
Youje0 <xhdckd12@gmail.com>
2022-12-12 17:25:24 +0900

    <add> eslint 및 기초세팅 변경

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Home';
 
 function Router() {
-  return <div>Router</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default Router;
