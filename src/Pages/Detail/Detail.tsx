d6d028a30459f4b13a13e88e131b3ee95e9e95ad
yeji <yejipooh@gmail.com>
2022-12-26 19:33:12 +0900

    <modifiy> 추가,삭제

--- src/Pages/Detail/Detail.tsx
+++ src/Pages/Detail/Detail.tsx
@@ -6,11 +6,34 @@ import useFetch from '../../hook/useFetch';
 import Comments from './Comments';
 import './Detail.scss';
 
+type CompanyType = {
+  categoryName: string;
+  corporationName: string;
+  image: string;
+  introduction: string;
+  url: string;
+  field: string;
+  detailIntro: string;
+  membersBenefits: string;
+  number: string;
+  introFile: string;
+  placeName: string;
+};
+
 function Detail() {
-  const company = useFetch('http://localhost:3004/company');
-  let { id } = useParams();
+  const [company, setCompany] = useState<CompanyType>();
+
+  useEffect(() => {
+    axios('http://localhost:8000/corporation/1').then((data) =>
+      setCompany(data.data.data)
+    );
+  }, []);
 
   console.log(company);
+
+  // const company = useFetch('http://localhost:3004/company');
+  // let { id } = useParams();
+
   return (
     <div className='container'>
       <div className='company-section'>
@@ -22,24 +45,26 @@ function Detail() {
         </div>
       </div>
 
-      <div className='Detail-area'>
-        {/* <img src={company.image} alt='banner' />
-        <h2>{company.corporationName}</h2>
-        <div>{company.detailIntro}</div>
-        <div>
-          <span className='Detail-intro'>업무분야</span>
-          <span>{company.field}</span>
+      {company && (
+        <div className='Detail-area'>
+          <img src={company.image} alt='banner' />
+          <h2>{company.corporationName}</h2>
+          <div>{company.detailIntro}</div>
+          <div>
+            <span className='Detail-intro'>업무분야</span>
+            <span>{company.field}</span>
+          </div>
+          <div>
+            <span className='Detail-intro'>홈페이지</span>{' '}
+            <span>{company.membersBenefits}</span>
+          </div>
+          <div>
+            <span className='Detail-intro'>연락처</span>{' '}
+            <span>{company.number}</span>
+          </div>
+          <div>{company.introFile}</div>
         </div>
-        <div>
-          <span className='Detail-intro'>홈페이지</span>{' '}
-          <span>{company.membersBenefits}</span>
-        </div>
-        <div>
-          <span className='Detail-intro'>연락처</span>{' '}
-          <span>{company.number}</span>
-        </div>
-        <div>{company.introFile}</div> */}
-      </div>
+      )}
 
       <Comments />
     </div>
--- src/hook/useFetch.tsx
+++ src/hook/useFetch.tsx
@@ -16,11 +16,11 @@ type CompanyType = {
 };
 
 export default function useFetch(url: string) {
-  const [data, setData] = useState<CompanyType>();
+  const [data, setData] = useState<CompanyType[]>([]);
 
   useEffect(() => {
     axios(url) //필요한 url이 바뀔예정이다
-      .then((data) => setData(data.data));
+      .then((data) => setData(data.data[0]));
   }, [url]);
 
   return data; //결국 필요한 data를 리턴해서 받을예정
